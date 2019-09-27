c85af6f3694a0cf8bf851d8460380331f74c59ac
aleodoni <aleodoni@gmail.com>
2019-09-27 12:50:28 -0300

    Updates

---
+++ precache-manifest.b9f33908a9a321d310a0d66519fb759c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824531de77fea105946928edb9d741e9",
+    "url": "/cafe-dif/index.html"
+  },
+  {
+    "revision": "8d84ed00fbf7bd25a48c",
+    "url": "/cafe-dif/static/js/2.29991f65.chunk.js"
+  },
+  {
+    "revision": "0439ed335e616495923b",
+    "url": "/cafe-dif/static/js/main.9cb52b4b.chunk.js"
+  },
+  {
+    "revision": "ed994da93b7164e9f8b0",
+    "url": "/cafe-dif/static/js/runtime-main.26e044af.js"
+  },
+  {
+    "revision": "8f53a40f1f5ac9e779db9564d916c3dc",
+    "url": "/cafe-dif/static/media/melitta1.8f53a40f.png"
+  },
+  {
+    "revision": "9efa03054d0c97dc55848e7cfa475572",
+    "url": "/cafe-dif/static/media/melitta2.9efa0305.png"
+  }
+]);
